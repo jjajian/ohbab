2ef2652530ab0058025abd6a8250ffe4a2616351
jjajian <143320605+jjajian@users.noreply.github.com>
2024-05-20 23:42:20 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -24,32 +24,48 @@ io.on('connection', (socket) => {
     console.log('New client connected');
 
     // 클래스 출발 시간 설정
-    socket.on('startMeal', async (classId) => {
+    socket.on('startMeal', (classId) => {
         const currentTime = new Date().toLocaleTimeString();
-        try {
-            const conn = await db.getConnection();
-            const query = `INSERT INTO times (class_id, time) VALUES (?, ?) ON DUPLICATE KEY UPDATE time = VALUES(time)`;
-            await conn.query(query, [classId, currentTime]);
-            io.emit('timeUpdated', { classId, currentTime });
-            conn.release(); // 연결 반환
-        } catch (err) {
-            console.log(err.message);
-        }
+        // getConnection을 사용하여 데이터베이스 연결을 가져옵니다.
+        db.getConnection((err, connection) => {
+            if (err) {
+                // 연결 중 오류가 발생한 경우
+                return console.error('Database connection failed: ', err);
+            }
+            // SQL 쿼리 실행
+            connection.query('INSERT INTO times (class_id, time) VALUES (?, ?) ON DUPLICATE KEY UPDATE time = ?', [classId, currentTime, currentTime], (err, results) => {
+                connection.release(); // 사용 완료된 연결을 pool로 반환합니다.
+                if (err) {
+                    return console.error(err.message);
+                }
+                // 모든 클라이언트에게 시간 업데이트 이벤트를 방송합니다.
+                io.emit('timeUpdated', { classId, currentTime });
+            });
+        });
     });
+    
 
     // 클래스 출발 시간 취소
     socket.on('cancelMeal', async (classId) => {
         try {
+            // 데이터베이스 연결을 가져옵니다.
             const conn = await db.getConnection();
+            
             const query = `DELETE FROM times WHERE class_id = ?`;
+            
+            // 쿼리를 실행합니다.
             await conn.query(query, [classId]);
+            
+            // 모든 클라이언트에게 시간 업데이트 이벤트를 방송합니다.
             io.emit('timeUpdated', { classId, currentTime: "" });
-            conn.release(); // 연결 반환
+            
+            // 연결 반환
+            conn.release();
         } catch (err) {
             console.log(err.message);
         }
     });
-
+    
     socket.on('disconnect', () => {
         console.log('Client disconnected');
     });
